18ee303803ffe54049c11e3caa1676fe679da048
John Steel <john@jskw.dev>
2023-04-07 14:28:04 -0400

    Whitespace fix

--- shahoot.sh
+++ shahoot.sh
@@ -6,20 +6,21 @@ QUESTION_TIME=10
 
 score() {
     if [[ "$ANSWER" =~ $1  ]]; then 
-        printf "That's correct! "
-        POINTS="$((QUESTION_TIME - (end_time - start_time) ))"
+        msg_prefix="That's correct!"
+        points="$((QUESTION_TIME - (end_time - start_time) ))"
     else
-        printf "That's incorrect. "
-        POINTS=0
+        msg_prefix="That's incorrect."
+        points=0
     fi
     SCORE=$((POINTS + SCORE))
-    printf "%d points!\n\n" $POINTS
+    printf "%s %d points!\n\n" "$msg_prefix" "$points"
 }
 
 for question in quiz1/*.question ; do
     cat "$question"
 
     start_time=$(date +%s)
+    printf "\nANSWER: "
     read -r -t $QUESTION_TIME ANSWER
     end_time="$(date +%s)"
 
